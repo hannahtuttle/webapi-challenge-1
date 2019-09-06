f14ed48436f4703e15b55a3e36a6efbd742f37a4
Hannah Tuttle <hannah@Hannahs-Air.PK5001Z>
2019-09-06 09:20:30 -0700

    added projects post, tested and working

--- data/helpers/projectRouter.js
+++ data/helpers/projectRouter.js
@@ -14,4 +14,15 @@ router.get('/', (req, res) => {
     })
 })
 
+router.post('/', (req, res) => {
+    const body = req.body
+    Data.insert(body)
+    .then(result => {
+        res.status(200).json(result)
+    })
+    .catch(error => {
+        res.status(500).json({error: "The project information could not be added."})
+    })
+})
+
 module.exports = router
