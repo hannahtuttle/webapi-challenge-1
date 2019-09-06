b5cff608bc6a2ad5c6e987fe781c16afe17d45ea
Hannah Tuttle <hannah@Hannahs-Air.PK5001Z>
2019-09-06 09:15:09 -0700

    add projects get request, tested and working

--- data/helpers/projectRouter.js
+++ data/helpers/projectRouter.js
@@ -1,1 +1,17 @@
 const express = require('express')
+
+const Data = require('./projectModel.js')
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    Data.get()
+    .then(result => {
+        res.status(200).json(result)
+    })
+    .catch(error => {
+        res.status(500).json({error: "The projects information could not be retrieved."})
+    })
+})
+
+module.exports = router
